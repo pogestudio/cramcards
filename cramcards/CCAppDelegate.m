2160f5cbdbb9962a4389a48ae04caa05f2110c09
PoGE Studio <carl-arvid@pogestudio.com>
2013-02-04 00:54:10 +0100

    added apptentive

--- cramcards/CCAppDelegate.m
+++ cramcards/CCAppDelegate.m
@@ -12,6 +12,11 @@
 
 #import "CCInAppPurchaseManager.h"
 
+#import "ATConnect.h"
+#import "ATAppRatingFlow.h"
+
+#define APP_ID @"com.pogestudio.cramcards"
+
 @implementation CCAppDelegate
 
 @synthesize managedObjectContext = _managedObjectContext;
@@ -30,6 +35,8 @@
     
     //initialize the shared inappmanager to create a payment observer (registers in the init)
     [CCInAppPurchaseManager sharedInstance];
+    
+    [self startApptentiveWithStartController:navigationController];
     return YES;
 }
 							
@@ -52,7 +59,14 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    UITabBarController *tabbarC = (UITabBarController*)self.window.rootViewController;
+    UIViewController *selectedVC = [tabbarC selectedViewController];
+    if ([selectedVC isKindOfClass:[UINavigationController class]]) {
+        UINavigationController *navCon = (UINavigationController*)selectedVC;
+        selectedVC = navCon.topViewController;
+    }
+    
+    [self restartApptentiveInViewController:selectedVC];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
@@ -156,4 +170,23 @@
     return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
 }
 
+#pragma mark Apptentive
+-(void)startApptentiveWithStartController:(UIViewController*)startVC
+{
+    NSString *kApptentiveAPIKey =
+    @"f37ac3d0e873e98c3f74193c3b59477dcb65b7fd5ce612c4d29b0cbeafe1870e";
+    ATConnect *connection = [ATConnect sharedConnection];
+    connection.apiKey = kApptentiveAPIKey;
+    
+    ATAppRatingFlow *sharedFlow =
+    [ATAppRatingFlow sharedRatingFlowWithAppID:APP_ID];
+    
+    [sharedFlow appDidLaunch:YES viewController:startVC];
+}
+-(void)restartApptentiveInViewController:(UIViewController*)restartVC
+{
+    ATAppRatingFlow *sharedFlow =
+    [ATAppRatingFlow sharedRatingFlowWithAppID:APP_ID];
+    [sharedFlow appDidEnterForeground:YES viewController:restartVC];
+}
 @end
